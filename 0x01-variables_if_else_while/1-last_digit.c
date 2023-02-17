88c64938f4cc62662b71a6bacde05b76fc5f8ad4
Trevor800 <jeynator780@gmail.com>
2023-02-17 13:33:52 -0800

    penny

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,10 +13,10 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
-printf("%d and is greater than 5\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n);
 else if (n == 0)
-printf("%d and is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n);
 else if (n < 6)
-printf("%d and is less than 6 and not 0\n", n);	
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
 return (0);
 }
