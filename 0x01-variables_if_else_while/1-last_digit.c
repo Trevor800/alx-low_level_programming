5a98822add65f9afd0c44b0b47c3fd61912d380a
Trevor800 <jeynator780@gmail.com>
2023-02-17 13:07:14 -0800

    vary

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,8 +17,6 @@ printf("%d and is greater than 5\n", n);
 else if (n == 0)
 printf("%d and is 0\n", n);
 else if (n < 6)
-printf("%d and is less than 6 and not 0\n", n);
-else
-printf("%d Last digit of\n", n);	
+printf("%d and is less than 6 and not 0\n", n);	
 return (0);
 }
