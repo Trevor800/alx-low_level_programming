934d5f0665c8c9a5a829af9756c70b2da48c97c8
Trevor800 <jeynator780@gmail.com>
2023-02-21 11:45:56 -0800

    quire

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - make alphabetprint x10 times
  * Return: void
  */
+
 void print_alphabet_x10(void)
+
 {
 	char c;
 	int i = 0;
@@ -11,9 +14,10 @@ void print_alphabet_x10(void)
 	while (i <= 9)
 	{
 	for (c = "a"; c <= "z"; c++)
-	}
+	{
 	_putchar(c);
-
+	}
 	_putchar("\n")
 	i++;
+	}
 }
