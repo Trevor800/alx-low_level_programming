b57617b0bbfe9396d55526310ccc7ab62f49c6ee
Trevor800 <jeynator780@gmail.com>
2023-02-21 14:41:07 -0800

    force

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+@@ -2,19 +2,19 @@
+
+/**
+ * add - adds two integers
+ * @a: integer 1
+ * @b: integer 2
+ * @n1: integer 1
+ * @n2: integer 2
+ *
+ * Description: give two integers, returns sum
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = b + b;
+	sum = n1 + n2;
+
+	return (sum);
+}
