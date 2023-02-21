46ca501aa82a8c205e68ad24c37fec378e7b4b5d
Trevor800 <jeynator780@gmail.com>
2023-02-21 10:08:58 -0800

    kar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *print_alphabet - makethe alphabet
+ *
+ * Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char alphabet = "n";
+	while (alphabet ="z");{
+	_putchar("\n", alphabet);
+	alphabet++;
+	}
+	_putchar("\n");
+	return(0);
+}
