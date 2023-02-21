93e39bc4c3a29e65ce21dacf574a25f98de81cd7
Trevor800 <jeynator780@gmail.com>
2023-02-21 11:01:54 -0800

    kar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -11,8 +11,8 @@ void print_alphabet(void)
 
         while (letter <= 'z')
         {
-                _putchar(letter);
-                letter++;
+        _putchar(letter);
+        letter++;
         }
         _putchar('\n');
 }
