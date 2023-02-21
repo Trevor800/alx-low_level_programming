65e6b654d7c9362448a7031c8587c4944a48d0fb
Trevor800 <jeynator780@gmail.com>
2023-02-21 14:24:50 -0800

    ctrl

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@ include "main.h"
  * print_sign - prints the sign of a number
  * @n: n is an integer
  *
- * Return: 1 (f +), 0 (if 0), -1 (if -)
+ * Return: 1 (if +), 0 (if zero), -1 (if -)
  */
 
 int print_sign(int n)
