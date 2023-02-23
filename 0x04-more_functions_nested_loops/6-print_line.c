96a88adaad3bdd791ec4d262f16cd0c9b1c26250
Trevor800 <jeynator780@gmail.com>
2023-02-23 15:34:55 -0800

    caro

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - function that prints line
+ * @n: parameter
+ */
+
+void print_line(int n)
+{
+int i = 1;
+while (i <= n)
+{
+_putchar(95);
+i++;
+}
+_putchar('\n');
+}
