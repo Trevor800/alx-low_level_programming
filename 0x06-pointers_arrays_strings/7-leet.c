76c45e6208c3d773eaa85ac5ac0777c43587f85c
Trevor800 <jeynator780@gmail.com>
2023-03-02 12:49:06 -0800

    render

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* leet - function that encodes a string in 1337
+* @s: function is the string to be encoded
+* Return: function as the resulting string;
+*/
+
+char *leet(char *s)
+{
+int i, j;
+char *a = "aAeEoOtTlL";
+char *b = "4433007711";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = b[j];
+}
+}
+}
+return (s);
+}
