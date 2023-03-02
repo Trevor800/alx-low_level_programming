8925756b6829d21148944cac7242043e5e30b8e3
Trevor800 <jeynator780@gmail.com>
2023-03-02 12:01:44 -0800

    render

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,22 +10,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+int i;
+int j;
 
-	i = 0
-	while (dest[i] != "\0")
-	{
-		i++;
-	}
-	j = 0
-	while (src[j] != "\0")
-	{
-		dest[i] = src[j];
-		i++;
-		j++;
-	}
+i = 0
+while (dest[i] != "\0")
+{
+i++;
+}
+j = 0
+while (src[j] != "\0")
+{
+dest[i] = src[j];
+i++;
+j++;
+}
 
-	dest[i] = "\0";
-	return (dest);
+dest[i] = "\0";
+return (dest);
 }
