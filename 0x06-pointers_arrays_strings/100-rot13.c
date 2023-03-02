c1573c023ee209749c7120932cadbad8b456523a
Trevor800 <jeynator780@gmail.com>
2023-03-02 12:51:29 -0800

    render

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* rot13 - functio encodes a string in rot13
+* @s: function is the string to be encoded
+* Return: function as the resulting string
+*/
+
+char *rot13(char *s)
+{
+int i, j;
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; a[j] != '\0'; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = b[j];
+break;
+}
+}
+}
+return (s);
+}
