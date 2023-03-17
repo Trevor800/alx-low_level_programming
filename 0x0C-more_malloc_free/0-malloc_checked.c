b7ec85ef65f864dbb4200643ed0157e48ea0ec7e
Trevor800 <jeynator780@gmail.com>
2023-03-17 12:33:10 -0700

    kardon

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: function of number of bytes to be malloced
+ * Return: function returns a void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (!ptr)
+		exit(98);
+	return (ptr);
+}
