78af7f0279d20e54462b6267fa81081448ed4e7b
Trevor800 <jeynator780@gmail.com>
2023-03-20 14:56:52 -0700

    like Macro

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,19 +1,6 @@
-#include <stdio.h>
-
 #ifndef _PI_H
 #define _PI_H
 
 #define PI 3.14159265359
 
 #endif
-
-int main(void)
-{
-    float a;
-    float r;
-
-    r = 98;
-    a = PI * r * r;
-    printf("%.3f\n", a);
-    return (0);
-}
