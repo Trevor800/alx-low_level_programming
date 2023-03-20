101b9aa00f3e8ee96b2109c6471bb7f9e06eea1a
Trevor800 <jeynator780@gmail.com>
2023-03-20 16:37:54 -0700

    like Macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -6,13 +6,3 @@
 #define ABS(x) ((x<0)?-(x):(x))         //(x<0)? *-1:
 
 #endif
-int main(void)
-{
-    int i;
-    int j;
-
-    i = ABS(-98) * 10;
-    j = ABS(98) * 10;
-    printf("%d, %d\n", i, j);
-    return (0);
-}
