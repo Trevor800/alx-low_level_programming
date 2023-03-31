6ab354c862622cd585603b1883634b74b31edc30
Trevor800 <jeynator780@gmail.com>
2023-03-30 17:19:43 -0700

    Jey

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,29 +1,24 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
+
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
+ * print_list - Prints all the elements of a list_t list
+ * @h: the list_t list
  *
- * Return: The number of nodes in h.
+ * Return: Number of codes
  */
 
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-
 		else
 			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
 		h = h->next;
+		nodes++;
 	}
-
 	return (nodes);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,26 +1,24 @@
-#ifndef LIST_H
-#define LIST_H
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include "lists.h"
+
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * print_list - Prints all the elements of a list_t list
+ * @h: the list_t list
  *
- * Description: singly linked list node structure
- * for Holberton project
-*/
-typedef struct list_s
+ * Return: Number of codes
+ */
+
+size_t print_list(const list_t *h)
 {
-        char *str;
-        unsigned int len;
-        struct list_s *next;
-} list_t;
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-#endif /* LIST_H */
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}}
