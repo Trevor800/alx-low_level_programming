2d5d20d5a2fc696f2faf1727e68917cb8298a1bc
Trevor800 <jeynator780@gmail.com>
2023-04-10 08:26:52 -0700

    manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,22 +9,22 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int check = 0;
-	unsigned int jey_c = 0;
+	unsigned int b_jey = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
 	while (b[check] != '\0')
 	{
 		if (b[check] == '0' || b[check] == '1')
 		{
-			jey_c <<= 1;
-			jey_c += b[check] - '0';
+			b_jey <<= 1;
+			b_jey += b[check] - '0';
 			check++;
 		}
 		else
 			return (0);
 	}
 
-	return (jey_c);
+	return (b_jey);
 }
