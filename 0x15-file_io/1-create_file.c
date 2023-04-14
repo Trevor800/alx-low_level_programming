5be442941f60e0e383f5a6d245e46ba3e1ab0ff0
Trevor800 <jeynator780@gmail.com>
2023-04-14 03:29:45 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	newFile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (text_content != NULL || 0)
+	if (text_content != NULL)
 	{
 		int len = 0;
 
