43b70a4bf6f265e881e2bd7dd14112863b122cd7
Trevor800 <jeynator780@gmail.com>
2023-04-14 13:45:04 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -31,6 +31,11 @@ while ((rd_bytes = read(src, buffer, BUFFER_SIZE)) > 0)
 		exit(99);
 	}
 }
+if (rd_bytes == -1)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+}
 if ((close(src)) == -1 || (close(final)) == -1)
 {
 	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src);
