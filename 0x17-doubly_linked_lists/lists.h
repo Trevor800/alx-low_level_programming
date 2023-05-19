d25cde065551b55d786375ff3dec0affb8b55541
Trevor800 <jeynator780@gmail.com>
2023-05-18 19:39:35 -0700

    Doubly linked lists updated

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,23 +1,24 @@
-#define LISTS_H
 #ifndef LISTS_H
+#define LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
- * @prev: function points to the previous node
- * @next: functin points to the next node
+ * @prev: points to the previous node
+ * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * for Holberton project
+ * 
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
@@ -28,7 +29,7 @@ void free_dlistint(dlistint_t *head);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-
-#endif
+#endif /* LISTS_H */
