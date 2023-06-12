a404d558c833692e88889eec56c69c614a42df4b
Trevor800 <jeynator780@gmail.com>
2023-06-12 06:31:20 -0700

    correction

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,12 +1,16 @@
-#define HASH_TABLES_H
 #ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
 
 /**
  * struct hash_node_s - Node of a hash table
+ *
  * @key: The key, string
  * The key is unique in the HashTable
  * @value: The value corresponding to a key
@@ -14,13 +18,14 @@
  */
 typedef struct hash_node_s
 {
-        char *key;
-        char *value;
-        struct hash_node_s *next;
+char *key;
+char *value;
+struct hash_node_s *next;
 } hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
+ *
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
@@ -28,12 +33,13 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-        unsigned long int size;
-        hash_node_t **array;
+unsigned long int size;
+hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 #endif
